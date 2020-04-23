0df7bd0146a882f7147c0b1d5f968e9d0db3f0e3
ShotaKitamura <kitamura@systemforest.com>
2020-04-23 18:26:35 +0900

    プログラムの調査内容をコメント

---
+++ soil-switchbot-explanation.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import binascii #バイナリ-ASCII変換用モジュール
+from bluepy.btle import Peripheral #bluepyがMACでインストールできないのでエラーが出てる模様をOBにインストールしてエラー確認。
+
+class SwitchBot:
+    def __init__(self, mac_address):
+        self.p = Peripheral(mac_address, "random") #BLEデバイスへ接続
+        hand_service = self.p.getServiceByUUID("cba20d00-224d-11e6-9fb8-0002a5d5c51b") #サービスオブジェクトの取得（SwitchBotの指定？
+        self.hand = hand_service.getCharacteristics("cba20002-224d-11e6-9fb8-0002a5d5c51b")[0]
+
+    def press(self):
+        self.hand.write(binascii.a2b_hex("570100")) #570101で倒す、570102で引くの命令
+        self.p.disconnect()
+
+    def on(self):
+        self.p.disconnect()
+        raise NotImplementedError()
+
+    def off(self):
+        self.p.disconnect()
+        raise NotImplementedError()
+
+if __name__ == "__main__":
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option("-a", "--address", dest="addr", help="MAC address of SwitchBot")
+    parser.add_option("-c", "--command", dest="cmd",  help="Command for SwitchBot (press|on|off)")
+    (options, args) = parser.parse_args()
+
+    getattr(SwitchBot(options.addr), options.cmd)()
+
