873b87052f84154f14b662f73c9dd1c3466778ef
ShotaKitamura <kitamura@systemforest.com>
2020-04-30 17:42:10 +0900

    読み取りプログラム追加版作成

---
+++ soil-switchbot-read.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import binascii
+from bluepy.btle import Peripheral
+
+class SwitchBot:
+    def __init__(self, mac_address):
+        self.p = Peripheral(mac_address, "random")
+        hand_service = self.p.getServiceByUUID("cba20d00-224d-11e6-9fb8-0002a5d5c51b")
+        self.hand = hand_service.getCharacteristics("cba20002-224d-11e6-9fb8-0002a5d5c51b")[0]
+        self.receive = hand_service.getCharacteristics("cba20003-224d-11e6-9fb8-0002a5d5c51b")[0]
+
+    def press(self):
+        self.hand.write(binascii.a2b_hex("570100"))
+        self.p.disconnect()
+
+    def on(self):
+        self.p.disconnect()
+        raise NotImplementedError()
+
+    def off(self):
+        self.p.disconnect()
+        raise NotImplementedError()
+
+    def notify(self):
+        receive001 = self.receive.read()
+        print('receive001:')
+        print(receive001)
+        receive002 = self.hand.read()
+        print('receive002:')
+        print(receive002)
+        
+
+
+if __name__ == "__main__":
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option("-a", "--address", dest="addr", help="MAC address of SwitchBot")
+    parser.add_option("-c", "--command", dest="cmd",  help="Command for SwitchBot (press|on|off)")
+    (options, args) = parser.parse_args()
+
+    getattr(SwitchBot(options.addr), options.cmd)()
